6a569ca645cc472bdb0195da35269afcbd02e6e2
Your Nameravindra6596 <you@example.comravipatil6596@gmail.com>
2021-05-09 22:43:34 +0530

    Delete Job

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -77,24 +77,25 @@ class JobsPage extends StatelessWidget {
   Widget _buildContents(BuildContext context) {
     final database = Provider.of<Database>(context, listen: false);
     return StreamBuilder<List<Job>>(
-        stream: database.jobsStream(),
-        builder: (context, snapshot) {
-          return ListItemsBuilder(
-            snapshot: snapshot,
-            itemBuilder: (context, job) => Dismissible(
-              key: Key('job-${job.id}'),
-              background: Container(color: Colors.red),
-              direction: DismissDirection.endToStart,
-              onDismissed: (direction) => _delete(context, job),
-              child: JobListTile(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        return ListItemsBuilder(
+          snapshot: snapshot,
+          itemBuilder: (context, job) => Dismissible(
+            key: Key('job-${job.id}'),
+            background: Container(color: Colors.red),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) => _delete(context, job),
+            child: JobListTile(
+              job: job,
+              onTap: () => EditJob.show(
+                context,
                 job: job,
-                onTap: () => EditJob.show(
-                  context,
-                  job: job,
-                ),
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
